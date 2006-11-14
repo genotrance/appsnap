6e41de8a59a808e1d38a9b73f33d29536dd46d06
ganeshjgd <ganeshjgd@b00821ba-6c1f-0410-8bfa-59e2bdee2fba>
2006-11-14 22:27:27 +0000

    Version 1.2

--- trunk/appsnap/appsnapgui.py
+++ trunk/appsnap/appsnapgui.py
@@ -1,9 +1,31 @@
 import guisetup
 import makegui
+import sys
+import time
 import version
 
-if __name__ == '__main__':
+OLD_STDOUT = None
 
+# Open debug log if -d specified
+def start_debug():
+    if len(sys.argv) == 2 and sys.argv[1] == '-d':
+        OLD_STDOUT = sys.stdout
+        sys.stdout = open('debug.log', 'a')
+        print '\n========================'
+        print time.asctime()
+        print '========================\n'
+
+# Close debug log if -d specified
+def end_debug():    
+    if len(sys.argv) == 2 and sys.argv[1] == '-d':
+        sys.stdout.close()
+        sys.stdout = OLD_STDOUT
+        
+# Main function
+if __name__ == '__main__':
+    # Start debug if requested
+    start_debug()
+    
     # Create a gui object
     gui = makegui.MakeGui(version.APPNAME + ' ' + version.APPVERSION, None, (guisetup.WIDTH, guisetup.HEIGHT))
 
@@ -12,3 +34,6 @@ if __name__ == '__main__':
 
     # Run the app
     gui.run()
+    
+    # End debug
+    end_debug()
