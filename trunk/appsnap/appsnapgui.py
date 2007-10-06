573247c540ec519d857067d4b6e3351b3bfe8747
ganeshjgd <ganeshjgd@b00821ba-6c1f-0410-8bfa-59e2bdee2fba>
2007-10-06 07:06:29 +0000

    Version 1.3.3

--- trunk/appsnap/appsnapgui.py
+++ trunk/appsnap/appsnapgui.py
@@ -2,42 +2,75 @@ import defines
 import guisetup
 import makegui
 import pycurl
+import StringIO
+import string
+import strings
 import sys
 import time
+import traceback
 import version
 import wx
+import wx.lib.dialogs
 
 OLD_STDOUT = None
+OLD_STDERR = None
+gui = None
 
-# Open debug log if -d specified, -s pipes to stdout
+# Redirect stdout and stderr to memory file
 def start_debug():
-    if len(sys.argv) == 2:
-        if sys.argv[1] == '--debug' or sys.argv[1] == '-d':
-            outfile = 'debug.log'
-        elif sys.argv[1] == '--stdout' or sys.argv[1] == '-s':
-            return
-    else:
-        outfile = 'nul'
-    
     OLD_STDOUT = sys.stdout
     OLD_STDERR = sys.stderr
-    sys.stdout = open(outfile, 'a')
+    sys.stdout = StringIO.StringIO()
     sys.stderr = sys.stdout
-    print '\n========================'
+    print '========================'
     print time.asctime()
     print '========================\n'
 
-# Close debug log if -d specified
+# Close memory file and restore stdout and stderr
 def end_debug():
-    if len(sys.argv) == 2 and (sys.argv[1] == '--stdout' or sys.argv[1] == '-s'):
-        return
+    if len(sys.argv) == 2 and (sys.argv[1] == '--debug' or sys.argv[1] == '-d'):
+        # Save to debug.log
+        dbg = open('debug.log', 'a')
+        dbg.write(sys.stdout.getvalue())
+        dbg.close()
     
     sys.stdout.close()
     sys.stdout = OLD_STDOUT
     sys.stderr = OLD_STDERR
+    
+# Handle uncaught exceptions
+def handle_exceptions(type, value, tb):
+    # Create traceback log
+    list = traceback.format_tb(tb, None) + traceback.format_exception_only(type, value)
+    tracelog = '\nTraceback (most recent call last):\n' + "%-20s%s" % (string.join(list[:-1], ""), list[-1])
+    sys.stderr.write(tracelog)
+    
+    # Create dialog message
+    message = strings.UNCAUGHT_EXCEPTION
+    message += '\n\n' + sys.stdout.getvalue()
+    
+    # Shutdown GUI
+    global gui
+    if gui != None:
+        gui.objects['frame'].Hide()
+        gui.objects['application'].ExitMainLoop()
+        gui = None
+
+    # Display in dialog
+    app = wx.App(False)
+    dlg = wx.lib.dialogs.ScrolledMessageDialog(None, message, strings.ERROR)
+    dlg.ShowModal()
+    
+    # Save to debug.log
+    dbg = open('debug.log', 'a')
+    dbg.write(sys.stdout.getvalue())
+    dbg.close()
 
 # Main function
 if __name__ == '__main__':
+    # Remap exception handler
+    sys.excepthook = handle_exceptions
+    
     # Start debug if requested
     start_debug()
     
@@ -54,6 +87,7 @@ if __name__ == '__main__':
 
     # Run the app
     gui.run()
+    gui = None
     
     # End debug
     end_debug()
